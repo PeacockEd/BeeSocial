3c90f02f14f3368c69dfeb2aa7608be85b452ce5
Edward P. Kelly <macromediaprod@yahoo.com>
2016-05-14 09:49:10 -0400

    Initial project commit.

---
+++ BeeSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeeSocial
+//
+//  Created by Edward P. Kelly on 5/14/16.
+//  Copyright © 2016 Edward P. Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'BeeSocial' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Firebase', '>= 2.5.1'
+
+end
